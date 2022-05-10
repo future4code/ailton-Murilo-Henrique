eed0c6381b8d6b7c7f54616cea1073a6914f5faa
Murilo Caun Henrique <mcchenrique2101@gmail.com>
2022-05-10 13:04:06 -0300

    Entrega exercicios de operadores

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,80 @@
+// EXERCICIOS DE INTERPRETAÇÃO:
+
+// 1.
+//      FALSE, FALSE, TRUE E BOOLEAN 
+
+// 2.
+//      ELE ESQUECEU DE TRANSFORMAR A INFORMÇÃO RECEBIDA DO USUARIO PARA NUMBER, ELA IRÁ COMO STRING ENTAO A SOMA IRA "SOMAR" AS DUAS "PALAVRAS" APARECENDO primeiroNumerosegundoNumero, exemplo: 10 + 20 = 1020
+
+// 3.
+/*  
+let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero = Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma)
+*/
+
+// EXERCICIOS DE ESCRITA DE CODIGO 
+
+//  1.
+/* 
+
+const suaIdade = Number (prompt ("Digite a sua Idade"))
+const idadeDoAmigo = Number (prompt ("Qual a idade do seu melhor amigo"))
+const resultado = suaIdade > idadeDoAmigo
+const diferencaIdade = suaIdade - idadeDoAmigo
+console.log (resultado)
+console.log (diferencaIdade)
+
+*/
+
+//  2.
+
+/*
+const numeroPar = Number (prompt ("Digite um numero PAR"))
+const numeroDivisor = 2
+
+console.log (numeroPar % numeroDivisor) 
+
+*/
+// COM NUMEROS PARES O RESULTADO SEMPRE VAI SER 0, POIS TODOS OS NUMEROS PARES SÃO PERFEITAMENTES DIVIDOS POR 2
+
+// COM NUMERO IMPARES, O RESULTADO SEMPRE VAI SER 1, POIS SEMPRE TERÁ RESTO NA DIVISÃO DE 2 POR NUMEROS IMPARES
+
+//  3.
+
+/*
+
+const suaIdade = Number (prompt ("Digite sua Idade"))
+const suaIdadeMeses = (suaIdade * 12)
+const suaIdadeDias = (suaIdadeMeses * 30)
+const suaIdadeHoras = (suaIdadeDias * 24)
+
+console.log ("Aproximadamente:" , suaIdadeMeses , "semanas")
+console.log ("Aproximadamente:" , suaIdadeDias , "dias")
+console.log ("Aproximadamente:" , suaIdadeHoras , "horas")
+
+*/
+
+//  4.
+
+/*
+const numeroUm = Number (prompt ("Digite um Numero")) 
+const numeroDois = Number (prompt ("Digite outro Numero")) 
+const resultado1 = (numeroUm > numeroDois)
+const resultado2 = (numeroUm === numeroDois)
+const restoDivisaoUm = (numeroUm % numeroDois)
+const restoDivisaoDois = (numeroDois % numeroUm)
+const resultado3 = (restoDivisaoUm === 0)
+const resultado4 = (restoDivisaoDois === 0)
+
+
+console.log (resultado1)
+console.log (resultado2)
+console.log (resultado3)
+console.log (resultado4)
+
+*/
+
