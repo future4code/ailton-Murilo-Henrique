72e82248f863219493cb53d08d03804d88333352
Murilo Caun Henrique <mcchenrique2101@gmail.com>
2022-05-23 14:27:07 -0300

    Exercicio 8 concluido

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -90,8 +90,16 @@ let resultado = {
 
 // EXERCÍCIO 08
 function retornaNPrimeirosPares(n) {
-   
-}
+    
+
+        let numerosPares = [];
+        for (let i = 0; numerosPares.length < n; i++) {
+            if (i % 2 == 0) {
+                numerosPares.push(i);
+            }
+        }
+        return numerosPares;
+    }
 
 // EXERCÍCIO 09
 function classificaTriangulo(ladoA, ladoB, ladoC) {
