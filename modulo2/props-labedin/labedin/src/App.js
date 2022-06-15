98ac6682161400f28c8477bb2461c83bc6fdf06b
Murilo Caun Henrique <mcchenrique2101@gmail.com>
2022-06-14 22:06:09 -0300

    atualização labedin

--- modulo2/props-labedin/labedin/src/App.js
+++ modulo2/props-labedin/labedin/src/App.js
@@ -1,93 +1,83 @@
-import React from 'react';
-import './App.css';
-import CardGrande from './components/CardGrande/CardGrande';
-import ImagemButton from './components/ImagemButton/ImagemButton';
-import ImagemPerfil from './img/foto-perfil.jpeg'
-import ImagemUnip from './img/logo-unip.png'
-import ImagemSenai from './img/logo-senai.png'
-import ImagemLabenu from './img/logo-labenu.png'
-import ImagemEmail from './img/logo-email.png'
-import ImagemEndereco from './img/logo-endereço.jpg'
-import CardPequeno from './components/CardPequeno/CardPequeno';
-import {DivButton} from './components/ImagemButton/ImagemButton'
-
-
+import React from "react";
+import "./App.css";
+import CardGrande from "./components/CardGrande/CardGrande";
+import VerMais from "./components/VerMais";
+import ImagemButton from "./components/ImagemButton/ImagemButton";
+import ImagemPerfil from "./img/foto-perfil.jpeg";
+import ImagemUnip from "./img/logo-unip.png";
+import ImagemSenai from "./img/logo-senai.png";
+import ImagemLabenu from "./img/logo-labenu.png";
+import ImagemEmail from "./img/logo-email.png";
+import ImagemEndereco from "./img/logo-endereço.jpg";
+import CardPequeno from "./components/CardPequeno/CardPequeno";
 
 function App() {
   return (
     <div className="App">
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem={ImagemPerfil} 
-          nome="Murilo Caun" 
+        <CardGrande
+          imagem={ImagemPerfil}
+          nome="Murilo Caun"
           descricao="Oi, eu sou o Murilo Caun. Tenho 24 anos, sou formado em Engenharia Mecanica e atualmente sou estudante de Desenvolvimento Web Full Stack na Labenu "
         />
-        
-        <ImagemButton 
-          imagem="https://cdn-icons-png.flaticon.com/512/271/271208.png" 
+
+        <VerMais
+          imagem="https://cdn-icons-png.flaticon.com/512/271/271208.png"
           texto="Ver mais"
         />
-         <CardPequeno
-          imagem={ImagemEmail} 
+        <CardPequeno
+          imagem={ImagemEmail}
           titulo="E-mail"
-          texto= "mcchenrique2101@gmail.com"
+          texto="mcchenrique2101@gmail.com"
         />
-         <CardPequeno 
-          imagem={ImagemEndereco} 
+        <CardPequeno
+          imagem={ImagemEndereco}
           titulo="Endereço:"
-          texto="Rua Bento Ortiz de Camargo Junior, Vinhedo/SP" 
-          
+          texto="Rua Bento Ortiz de Camargo Junior, Vinhedo/SP"
         />
-        
       </div>
 
       <div className="page-section-container">
         <h2>Formações Acadêmicas</h2>
-        <CardGrande 
-          imagem={ImagemLabenu} 
-          nome="Labenu" 
-          descricao="Cursando Desenvolvimento Web Full Stack, conclusão prevista em Novembro 2022" 
+        <CardGrande
+          imagem={ImagemLabenu}
+          nome="Labenu"
+          descricao="Cursando Desenvolvimento Web Full Stack, conclusão prevista em Novembro 2022"
         />
 
-        <CardGrande 
-          imagem={ImagemUnip} 
-          nome="Universidade Paulista" 
-          descricao="Bacharelado em Engenharia Mecânica, concluido em Dezembro de 2020" 
+        <CardGrande
+          imagem={ImagemUnip}
+          nome="Universidade Paulista"
+          descricao="Bacharelado em Engenharia Mecânica, concluido em Dezembro de 2020"
         />
-          <CardGrande
-          imagem={ImagemSenai} 
-          nome="SENAI" 
-          descricao="Curso Tecnico em Mecâtronica, concluído em Dezembro de 2015" 
+        <CardGrande
+          imagem={ImagemSenai}
+          nome="SENAI"
+          descricao="Curso Tecnico em Mecâtronica, concluído em Dezembro de 2015"
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
-       
-        
-        <DivButton>
-           
-        <ImagemButton 
-          imagem="https://e7.pngegg.com/pngimages/778/732/png-clipart-logo-linkedin-computer-icons-facebook-facebook-blue-text.png" 
-          texto="Linkedin" 
-          link= "https://www.linkedin.com/in/murilo-caun-henrique-979430206/" 
-        />     
-         <ImagemButton 
-          imagem="https://cdn-icons-png.flaticon.com/512/25/25231.png" 
-          texto="GitHub"
-          link= "https://github.com/MuriloCaun" 
-          
-        />
-            <ImagemButton 
-          imagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png" 
-          texto="Instagram"
-          link= "https://www.instagram.com/murilo_k1/" 
-          
-        />
-      
-        </DivButton>   
-        
+
+        <div>
+          <ImagemButton
+            imagem="https://e7.pngegg.com/pngimages/778/732/png-clipart-logo-linkedin-computer-icons-facebook-facebook-blue-text.png"
+            texto="Linkedin"
+            link="https://www.linkedin.com/in/murilo-caun-henrique-979430206/"
+          />
+          <ImagemButton
+            imagem="https://cdn-icons-png.flaticon.com/512/25/25231.png"
+            texto="GitHub"
+            link="https://github.com/MuriloCaun"
+          />
+          <ImagemButton
+            imagem="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/2048px-Instagram_logo_2016.svg.png"
+            texto="Instagram"
+            link="https://www.instagram.com/murilo_k1/"
+          />
+        </div>
       </div>
     </div>
   );
--- modulo2/props-labedin/labedin/src/components/VerMais.js
+++ modulo2/props-labedin/labedin/src/components/VerMais.js
@@ -2,35 +2,32 @@ import React from "react";
 import styled from "styled-components";
 
 export const DivButton = styled.div`
-  width: 50%;
-  margin: 10px auto;
-  gap: 20px;
   display: flex;
   flex-direction: column;
   align-items: center;
   border: 1px solid black;
+  width: 40%;
+  margin: 10px auto;
+  gap: 20px;
   border-radius: 50px 50px 50px 50px;
 `;
-export const ImagensButton = styled.img`
-  width: 40px;
-
-  margin-top: 10px;
-  align-items: center;
-  justify-content: center;
-  display: flex;
+const VerMais = styled.img`
+  width: 30px;
+  margin-right: 10px;
+  margin-top: 20px;
 `;
 
 const VerMaisTexto = styled.a`
-  margin-bottom: 10px;
+  margin-bottom: 15px;
 `;
 
-function ImagemButton(props) {
+function SetaDiv(props) {
   return (
     <DivButton>
-      <ImagensButton alt="" src={props.imagem} />
+      <VerMais alt="" src={props.imagem} />
       <VerMaisTexto href={props.link}>{props.texto}</VerMaisTexto>
     </DivButton>
   );
 }
 
-export default ImagemButton;
+export default SetaDiv;
